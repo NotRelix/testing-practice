5ab9a267b1934bb92634e58c37b6618064833771
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-04-28 13:42:41 +0800

    feat: add caesar cipher function with tests

--- src/string.js
+++ src/string.js
@@ -13,3 +13,22 @@ export function reverseString(text) {
   }
   return reversedWord;
 }
+
+export function caesarCipher(text, key) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  const modifiedText = text.toLowerCase().split("");
+  const encryptedMessage = modifiedText.map((letter) => {
+    let alphabetIndex = null;
+    alphabet.forEach((alphabetLetter, index) => {
+      if (letter === alphabetLetter) {
+        alphabetIndex = index;
+      }
+    });
+    if (alphabetIndex === null) {
+      return [" "];
+    }
+    const newAlphabetIndex = (alphabetIndex + key) % alphabet.length;
+    return alphabet[newAlphabetIndex];
+  })
+  return encryptedMessage.join("").toUpperCase();
+}
--- tests/string.test.js
+++ tests/string.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from "../src/string.js"
+import { caesarCipher, capitalize, reverseString } from "../src/string.js"
 
 describe("Capitalize String", () => {
   test("Capitalize First Letter", () => {
@@ -19,3 +19,17 @@ describe("Reverse String", () => {
     expect(reverseString("testing once again")).toBe("niaga ecno gnitset");
   });
 });
+
+describe("Caesar Cipher Encryption", () => {
+  test("Encrypt Simple Text", () => {
+    expect(caesarCipher("abc", 1)).toBe("BCD");
+  });
+
+  test("Encrypt Multiple Words", () => {
+    expect(caesarCipher("abc def ghij klmnop", 1)).toBe("BCD EFG HIJK LMNOPQ");
+  });
+
+  test("Encrypt With Different Key", () => {
+    expect(caesarCipher("abc def ghij klmnop", 7)).toBe("HIJ KLM NOPQ RSTUVW");
+  });
+})
